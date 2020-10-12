f57ccacdf0ce9bf9f23e414ff42f8bc47413a42e
scram91 <67920223+scram91@users.noreply.github.com>
2020-10-12 13:52:38 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,40 +3,60 @@
 
 using namespace std;
 
-class Employee{
+class Employee {
 public:
 	string firstName;
 	string lastName;
+	double hoursScheduled;
 	double hoursWorked;
-	
+	int type;
 private:
 	int id;
-	
-public: Employee(string FirstName, string LastName, int ID){
-		firstName = FirstName;
-		lastName = LastName;
-		id = ID;
-	
-	void setID(int x){
-		id = x;
-	}
-	int getID(){
-		return id;
-	}
+
+public: Employee(string FirstName, string LastName, int ID) {
+	firstName = FirstName;
+	lastName = LastName;
+	id = ID;
 }
-	
+	  void setID(int x) {
+		  id = x;
+	  }
+	  int getID() {
+		  return id;
+	  }	  
+	  int setEmployeeType(int x) {
+		  type = x;
+
+		  return type;
+	  }
 };
 
-class Salary : Employee{
-	
+class printSchedule {
+private:
+	string dayName;
+	int day;
+	string shift;
+	double shifthours;
 };
 
-class Hourly : Employee{
-	
+class Salary : public Employee {
+public: 
+	double salaryRate;
+
+	double calcSalary(double x) {
+		Employee::hoursWorked = x;
+
+		double netPay = salaryRate * x;
+		double taxes = netPay * 0.10; //tax rate for salaried employees is 0.10
+
+	}
 };
 
-class EmployeeSchedule : Employee{
-	
+class Hourly : public Employee{
+public:
+	double hourlyRate;	
+	//tax rate for hourly employees is 0.04
+	double calcWage
 };
 
 int main() {
