70c5e6319312412499f9b2f24a21f3b349dde259
scram91 <67920223+scram91@users.noreply.github.com>
2020-10-12 13:41:45 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,41 @@
 
 using namespace std;
 
+class Employee{
+public:
+	string firstName;
+	string lastName;
+	double hoursWorked;
+	
+private:
+	int id;
+	
+public: Employee(string FirstName, string LastName, int ID){
+		firstName = FirstName;
+		lastName = LastName;
+		id = ID;
+	
+	void setID(int x){
+		id = x;
+	}
+	int getID(){
+		return id;
+	}
+}
+	
+};
+
+class Salary : Employee{
+	
+};
+
+class Hourly : Employee{
+	
+};
+
+class EmployeeSchedule : Employee{
+	
+};
 
 int main() {
 
