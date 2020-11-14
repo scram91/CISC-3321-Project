16e25f29632f0b8c67d81336b911613af6111b87
scram91 <67920223+scram91@users.noreply.github.com>
2020-11-14 16:38:16 -0600

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <iostream>
-using namespace std;
 #include <string>
+using namespace std;
+
 
 // Employee abstract class
 class Employee {
+
 public:
 	// show employee information
 	virtual void showInfo() = 0;
@@ -15,9 +17,9 @@ public:
 	// employee number
 	int m_Id;
 
-	// employee name
-	string m_Name;
-
 	// department number
 	int m_DeptId;
+
+	string m_Name;
+
 };
