7602ad5f0ec9efd053452c989f199d0f88ae64d5
scram91 <67920223+scram91@users.noreply.github.com>
2020-11-14 16:37:42 -0600

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,18 +1,18 @@
 #pragma once
 #include <iostream>
-using namespace std;
 #include "Employee.h"
 #include "HR.h"
 #include "Manager.h"
 #include "Sales.h"
 #include "Engineer.h"
+#include "Schedule.h"
 #include <fstream>
 #define FILENAME "empFile.txt"
 
+using namespace std;
+
 class EmployeeManagement {
 public:
-	EmployeeManagement();
-
 	// display the main menu
 	void displayMenu();
 
@@ -22,44 +22,11 @@ public:
 	// add employee
 	void addEmployee();
 
-	// save file
-	void save();
-
-	// get the number of employee in the file
-	int getEmpNum();
-
-	// Initialize employee
-	void initEmp();
-
 	// diaplay employee
 	void displayEmp();
 
-	// delete employee
-	void delEmp();
+	//Add employee schedule
+	void addSchedule();
 
-	// check if an employee exists, if exists return the index in the array, or return -1
-	int isExist(int id);
-
-	// modify employee
-	void modifyEmp();
-
-	// find employee
-	void findEmp();
-
-	// sort employee by ID
-	void sortEmp();
-
-	// clear file
 	void clearFile();
-
-	// total number of employee
-	int m_EmpNum;
-
-	// whether the file is empty
-	bool m_IsFileEmpty;
-
-	// Employee array pointer
-	Employee** m_EmpArray;
-
-	~EmployeeManagement();
 };
